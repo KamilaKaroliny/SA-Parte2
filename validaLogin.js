784280299e24ec2d76aeb946c407f2ae62141740
João Medeiros <joao_m_medeiros@estudante.sesisenai.org.br>
2025-04-01 12:53:54 -0300

    add - atualizando valida js

--- validaLogin.js
+++ validaLogin.js
@@ -7,32 +7,18 @@ function validarFormulario() {
 
     //Processar os dados (obrigatoriedades)!
 
-    if(!nome){
+    if(!email){
         alert('Por favor, preencha o nome.');
         return;
     }else{
-        console.log(nome);
+        console.log(email);
     }
 
-    if(!cpf || cpf.length !==11 || isNaN(cpf)){
-        alert('Por favor, insira um CPF válido com 11 digitos');
+    if(!senha){
+        alert('Por favor, preencha o nome.');
         return;
     }else{
-        console.log(cpf);
-    }
-
-    if(isNaN(idade) || idade < 21 && idade > 0){
-        alert('A idade deve ser um número maior que trinta');
-        return;
-    }else{
-        console.log(idade);
-    }
-
-    if(isNaN(experiencia) || experiencia < 3 && experiencia > 0){
-        alert('Os anos de experiência devem ser maior que três ou igual');
-        return;
-    }else{
-        console.log(experiencia);
+        console.log(senha);
     }
 
     //Devolver
