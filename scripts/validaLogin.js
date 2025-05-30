88b70d9c0b3e1b118c3742ceb1727e479a1b8536
Daniels_gnoatto <daniel_gnoatto@estudante.sesisenai.org.br>
2025-05-30 09:23:03 -0300

    Eliminando a parte de marcação, por conta de problemas na validação do código

--- scripts/validaLogin.js
+++ scripts/validaLogin.js
@@ -52,20 +52,3 @@ function login() {
 function esqueceusenha2() {
   window.location.href = "esqueceusenha2.html";
 }
-
-// Código para fazer com que apareça a tela flutuante
-
-// document.getElementById("openMarcacao").onclick = function () {
-//   document.getElementById("marcacaoModal").style.display = "block";
-// }
-
-// document.querySelector(".closeBtn").onclick = function () {
-//   document.getElementById("marcacaoModal").style.display = "none";
-// }
-
-// window.onclick = function (event) {
-//   const modal = document.getElementById("marcacaoModal");
-//   if (event.target === modal) {
-//     modal.style.display = "none";
-//   }
-
