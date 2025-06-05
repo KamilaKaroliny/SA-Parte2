987bb7c287a61f7d6e8241d0a656faf403688dba
Kamila Karoliny dos Santos <kamila_k_santos@estudante.sesisenai.org.br>
2025-06-05 08:42:36 -0300

    add - começo da validação do esqueceusenha2 no JS

--- scripts/validaLogin.js
+++ scripts/validaLogin.js
@@ -89,6 +89,44 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+//validação esqueceusenha2
+document.addEventListener("DOMContentLoaded", function () {
+  const formulario = document.getElementById("esqueceusenha2");
+
+  formulario.addEventListener("submit", function (i) {
+    i.preventDefault();
+
+    let valido = true;
+
+    // para limpar os erros
+    document.getElementById("erroCodigo").textContent = "";
+    document.getElementById("erroNovaSenha").textContent = "";
+
+    const codigo = document.getElementById("codigo").value.trim();
+    const novaSenha = document.getElementById("novaSenha").value.trim();
+
+    console.log(codigo);
+    console.log(novaSenha);
+
+    if (codigo.length < 6) {
+      document.getElementById("erroCodigo").textContent = "O Codigo deve ter pelo menos 6 caracteres";
+      valido = false;
+    }
+    if (novaSenha.length < 6) {
+      document.getElementById("erroNovaSenha").textContent = "A Senha deve ter pelo menos 6 caracteres";
+      valido = false;
+    }
+
+    if (valido) {
+      alert("Senha redefinida com sucesso!");
+      formulario.reset();
+      window.location.href = "login.html";
+    }
+
+  });
+
+});
+
 //mudança de pagina
 function paginaInicial() {
   window.location.href = "paginaInicial.html";
