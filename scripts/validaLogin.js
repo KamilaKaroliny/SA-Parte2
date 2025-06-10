3d3c7185e282f50fa212ba3c7cccb72557e76ac1
João Medeiros <joao_m_medeiros@estudante.sesisenai.org.br>
2025-06-10 12:29:16 -0300

    add - CONSEGUI FAZER O GRAFICO!!

--- scripts/validaLogin.js
+++ scripts/validaLogin.js
@@ -215,5 +215,34 @@ function esqueceusenha2() {
 }
 
 
+//adição de gráfico relatório maquinistas
 
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var dataTable = new google.visualization.DataTable();
+  dataTable.addColumn('string', 'Categoria');
+  dataTable.addColumn('number', 'Valor');
+  dataTable.addRows([
+      ['Item A', 10],
+      ['Item B', 15],
+      ['Item C', 5]
+  ]);
+
+  var options = {
+    title: 'Gráfico de Pizza',
+    backgroundColor: 'white',
+    titleTextStyle: {color: 'black'},
+    pieSliceText: 'value',
+    slices: {
+        0: { color: 'red' },
+        1: { color: 'blue' },
+        2: { color: 'green' }
+    }
+};
+
+  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+  chart.draw(dataTable, options);
+}
 
