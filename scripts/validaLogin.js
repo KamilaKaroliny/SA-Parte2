871a3df41af9806b1c36778aeb88cde43e6e05cb
Kamila Karoliny dos Santos <kamila_k_santos@estudante.sesisenai.org.br>
2025-06-12 08:53:18 -0300

    add - separando o js

--- scripts/validaLogin.js
+++ scripts/validaLogin.js
@@ -40,55 +40,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-//validação esqueceusenha
-document.addEventListener("DOMContentLoaded", function () {
-  const esqueceusenha = document.getElementById("esqueceusenha");
-
-  esqueceusenha.addEventListener("submit", function (s) {
-    s.preventDefault();
-
-    let valido = true;
-
-    // para limpar os erros
-    document.getElementById("erroNome").textContent = "";
-    document.getElementById("erroEmail").textContent = "";
-    document.getElementById("erroId").textContent = "";
-
-    const nome = document.getElementById("nome").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const id = document.getElementById("id").value.trim();
-
-    console.log(nome);
-    console.log(email);
-    console.log(id);
-
-    if (nome.length < 3) {
-      document.getElementById("erroNome").textContent = "O nome deve ter pelo menos 3 caracteres";
-      valido = false;
-    }
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!emailRegex.test(email)) {
-      document.getElementById("erroEmail").textContent = "E-mail está inválido";
-      valido = false;
-    }
-
-    if (id.length < 6) {
-      document.getElementById("erroId").textContent = "O ID deve ter pelo menos 6 caracteres";
-      valido = false;
-    }
-
-    if (valido) {
-      alert("Senha redefinida com sucesso!");
-      formulario.reset();
-      window.location.href = "esqueceusenha2.html";
-    }
-
-  });
-
-});
-
 //validação esqueceusenha2
 document.addEventListener("DOMContentLoaded", function () {
   const esqueceusenha2 = document.getElementById("esqueceusenha2");
