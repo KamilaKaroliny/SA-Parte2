2f5c81707777919db62ac78146bb7f207aa5d94e
Daniels-gnoatto <daniel_gnoatto@estudante.sesisenai.org.br>
2025-06-12 20:32:59 -0300

    DEU CERTO!!!!!! Consegui fazer aparecer o gráfico do no relatório, agora é ajeitar as cores e os números

--- scripts/graficoCircular1970.js
+++ scripts/graficoCircular1970.js
@@ -2,10 +2,10 @@ const ctx = document.getElementById('chartHorario').getContext('2d');
   new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['00:00', '00:00', '00:00', '00:00'], // 
+      labels: ['00:00', '00:00', '00:00', '00:00'], // horários no eixo x
       datasets: [{
         label: 'Qtde de viagens',
-        data: [0, 1, 2, 3], // 
+        data: [0, 1, 2, 3, 4], // valores das barras
         backgroundColor: [
           'rgba(54, 162, 235, 0.6)',
           'rgba(54, 162, 235, 0.6)',
@@ -14,6 +14,12 @@ const ctx = document.getElementById('chartHorario').getContext('2d');
         ],
         borderColor: 'rgba(54, 162, 235, 1)',
         borderWidth: 1
-    }]}
+      }]
+    },
+    options: {
+      scales: {
+        y: { beginAtZero: true },
+        x: { title: { display: true, text: 'Horário' } }
+      },
     }
-)
+    })
