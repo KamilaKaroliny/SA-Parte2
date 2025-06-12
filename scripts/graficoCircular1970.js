be20d21a966762ff41b165fbff1c64155c03548b
Daniels-gnoatto <daniel_gnoatto@estudante.sesisenai.org.br>
2025-06-12 20:21:40 -0300

    Reescrevi o código selecionando o tipo de gráfico de barra, as cores eu não ajeitei e nem os número reais, só coloquei para ter uma idéia se daria certo

---
+++ scripts/graficoCircular1970.js
@@ -0,0 +1,18 @@
+const ctx = document.getElementById('chartHorario').getContext('2d');
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['00:00', '00:00', '00:00', '00:00'], // 
+      datasets: [{
+        label: 'Qtde de viagens',
+        data: [0, 1, 2, 3], // 
+        backgroundColor: [
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(54, 162, 235, 0.6)'
+        ],
+        borderColor: 'rgba(54, 162, 235, 1)',
+        borderWidth: 1
+      }]
+    };
