91d24ed3c814a5758e4da26628c088adabe3a7e1
kamila karoliny dos santos <kamila_k_santos@estudante.sesisenai.org.br>
2025-11-07 10:01:55 -0300

    add - alteração na conexão, criação de includes para incluir a aba de notificação nas telas iniciais

--- public/admin/paginaInicial.php
+++ public/admin/paginaInicial.php
@@ -11,40 +11,7 @@
 
 <body>
 
-    <!-- sininho e checkbox de notificação -->
-    <label>
-        <input class="noticacao" type="checkbox">
-        <div class="toggle" >
-            <img id="sininho2" src="../../assets/icons/sininho.png" alt="Notificação">
-        </div>
-
-        <div class="notificacoes">
-            <h2 class="titulo">NOTIFICAÇÕES</h2>
-
-            <div>
-                <h2 class="tituloNoti">
-                    Atraso detectado:
-                </h2><br>
-    
-                <h3 class="noti">
-                    O trem #1845 está 15 minutos atrasado devido a congestionamento 
-                    na linha. Atualização em breve.
-                </h3>
-            </div>
-            
-            <div>
-                <h2 class="tituloNoti">
-                    Manutenção agendada:
-                </h2><br>
-
-                <h3 class="noti">
-                    O Trem #1429 será retirado de operação para inspeção programada 
-                    às 22h00. Substituição já programada.
-                </h3>
-            </div>
-            
-        </div>
-    </label>
+ <?php include("../../includes/abaNotificacaoADM.php"); ?>
 
     <!-- div flex que comporta a logo -->
     <div id=flex2>
