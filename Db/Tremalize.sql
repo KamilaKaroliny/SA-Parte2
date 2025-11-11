8d00f228e83489ffa802ddf0ca55c6e229915777
daniels-gnoatto <daniel_gnoatto@estudante.sesisenai.org.br>
2025-11-11 11:25:55 -0300

    Atualizando os dados com o banco para funcionar a parte do editar e pegar as informações da marcação

--- Db/Tremalize.sql
+++ Db/Tremalize.sql
@@ -1,7 +1,7 @@
-create database tremalize_db;
-use tremalize_db;
+CREATE DATABASE tremalize_db;
+USE tremalize_db;
 
-/*Tabela de usuarios*/
+-- Tabela de usuários
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(125) NOT NULL,
@@ -12,19 +12,19 @@ CREATE TABLE usuarios (
     data_nascimento DATE,
     telefone VARCHAR(20),
     idade INT,
-    cep VARCHAR(10) NULL,
-    rua VARCHAR(255)  NULL,
-    cidade VARCHAR(100)  NULL,
-    bairro VARCHAR(100)  NULL, 
-    numero INT  NULL,
+    cep VARCHAR(10),
+    rua VARCHAR(255),
+    cidade VARCHAR(100),
+    bairro VARCHAR(100),
+    numero INT,
     complemento VARCHAR(255),
     data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     foto_perfil VARCHAR(255) DEFAULT 'default.jpg'
 );
 
-
+-- Tabela de trens
 CREATE TABLE trem (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     ultimaManutencao DATE NOT NULL,
     proximaManutencao DATE NOT NULL,
     distancia DECIMAL(10,2) NOT NULL,
@@ -34,28 +34,34 @@ CREATE TABLE trem (
     combustivelMaximo DECIMAL(10,2) NOT NULL,
     capacidadeMaxima INT NOT NULL
 );
-    
-create table sensor(
-    id int AUTO_INCREMENT PRIMARY KEY,
-    tipo varchar(225) not null,
-    descricao varchar(225) not null,
-    status_sensor varchar(225) not null
+
+-- Tabela de marcações
+CREATE TABLE marcacao (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    localizacao VARCHAR(225) NOT NULL,
+    icone ENUM('Acidente', 'Obras', 'Quebra') NOT NULL
 );
-     
-create table sensor_data(
-   id int AUTO_INCREMENT PRIMARY KEY,
-   valor bigint not null,
-   data_hora datetime not null,
-   id_sensor int not null,
+
+-- Tabela de sensores
+CREATE TABLE sensor (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo VARCHAR(225) NOT NULL,
+    descricao VARCHAR(225) NOT NULL,
+    status_sensor VARCHAR(225) NOT NULL
+);
+
+-- Tabela de dados dos sensores
+CREATE TABLE sensor_data (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   valor BIGINT NOT NULL,
+   data_hora DATETIME NOT NULL,
+   id_sensor INT NOT NULL,
    FOREIGN KEY (id_sensor) REFERENCES sensor (id)
 );
 
-     
-/*Usuarios inceridos*/
-use tremalize_db;
-INSERT into usuarios( nome, senha, credencial, email, tipo, data_nascimento, telefone, idade) VALUES
-('Icaro', '$2y$10$C1PHSSorIqP0NAj84qC0tO1KJOszl4cfEadt3g1kTZhNPnXmY6AYi','X9Y4Z6A1B4','icaro@administrador.com','ADM','2001-10-22', '4799955282', 24),
-('Clodoaldo Kowalski', '$2y$10$0bEiISHnAtiCfCt7WVdWQOtpnPhzzZG6nfuEVAkTpEkG7A5Zv.Hhe', 'X9Y4Z6A1B3','clodoaldo@maquinista.com','USER','1981-10-23','21954321098',71),
-('Jarbas Andrade', '$2y$10$DDqAqp/FfIRp7/G8SXL2k.SJ80smRuZ/.Nf4DyurIQba9jyo76uYa',' MSJ870NSHXU6','jarbas@maquinista.com','USER','1991-10-12', '21998565489', 34);
-
-
+-- Inserção de usuários
+INSERT INTO usuarios (nome, senha, credencial, email, tipo, data_nascimento, telefone, idade)
+VALUES
+('Icaro', '$2y$10$C1PHSSorIqP0NAj84qC0tO1KJOszl4cfEadt3g1kTZhNPnXmY6AYi', 'X9Y4Z6A1B4', 'icaro@administrador.com', 'ADM', '2001-10-22', '4799955282', 24),
+('Clodoaldo Kowalski', '$2y$10$0bEiISHnAtiCfCt7WVdWQOtpnPhzzZG6nfuEVAkTpEkG7A5Zv.Hhe', 'X9Y4Z6A1B3', 'clodoaldo@maquinista.com', 'USER', '1981-10-23', '21954321098', 71),
+('Jarbas Andrade', '$2y$10$DDqAqp/FfIRp7/G8SXL2k.SJ80smRuZ/.Nf4DyurIQba9jyo76uYa', 'MSJ870NSHXU6', 'jarbas@maquinista.com', 'USER', '1991-10-12', '21998565489', 34);
