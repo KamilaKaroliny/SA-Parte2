a567ea6e5112fcfca03c845b8d3e5f448421dc24
Kamila karoliny dos Santos <kamila_k_santos@estudante.sesisenai.org.br>
2025-11-18 12:13:19 -0300

    add - alterações e criação da tela de redefinir senha

--- public/Login/redefinirSenha.php
+++ public/Login/redefinirSenha.php
@@ -3,33 +3,31 @@
    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="../../style/style.css">
     <title>Redefinir senha</title>
 
 <body>
     <div class="flex">
 
         <div id="formulario">
-            <img id = "logo" src="./assets/icons/logoTremalize.png" alt="Logo Tremalize">
+            <img id = "logo" src="../../assets/icons/logoTremalize.png" alt="Logo Tremalize">
             <h1 id="padding">Alterar senhas</h1>
 
-            <form action="" id="gestorForm" method="POST">
-        
-                <label for="email"></label><br>
-                <input class="esticadinho" type="email" name="email" id="email" value="" placeholder="E-mail">
-                <div class="erro" id="erroEmail">
+            <form action="" id="alterarForm" method="POST">
     
                 <label for="senha"></label><br>
                 <input class="esticadinho" name="senha" id="senha" value="" placeholder="Senha">
                 <div class="erro" id="erroSenha">
 
-                <div id="esqueceuSenhaa">
-                    <a href="public/login/esqueceuSenha.php">Esqueceu a senha?</a>
-                </div>
-                <br>
-                <br>
+                <label for="novaSenha"></label><br>
+                <input class="esticadinho" name="senha" id="senha" value="" placeholder="Senha">
+                <div class="erro" id="erroSenha">
+
+                <label for="novaSenha2"></label><br>
+                <input class="esticadinho" name="senha" id="senha" value="" placeholder="Senha">
+                <div class="erro" id="erroSenha">
                 
-                <button id='button1' type="submit">Enviar</button>
+                <button id='button1' type="submit">Alterar</button>
                 
             </form>
 
