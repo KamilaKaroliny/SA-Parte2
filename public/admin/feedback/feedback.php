7618bb1cee9cac03b1c54a7b6e4479e00dfd849b
Amanda <amanda_moller@estudante.sesisenai.org.br>
2025-11-27 07:44:58 -0300

    Criando feedback

---
+++ public/admin/feedback/feedback.php
@@ -0,0 +1,90 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../style/style.css">
+    <script src="../../scripts/validaLogin.js"></script>
+    <script src="../../scripts/validaCadastro.js"></script>
+    <script src="../../scripts/validaEsqueceusenha.js"></script>
+    <script src="../../scripts/validaEsqueceusenha2.js"></script>
+    <script src="../../scripts/graficoRelatorioM.js"></script>
+    <title>Feedback</title>
+</head>
+<body>
+    <!-- Parte de cima do feedback (seta, casa e logo tremalize -->
+    <div id="flex4">
+        <div class="meio1">
+            <a href="paginaInicial.php">
+                <img id="seta" src="../../assets/icons/seta.png" alt="seta">
+            </a>
+        </div>
+
+        <div class="meio1">
+            <img id="logo4" src="../../assets/icons/logoTremalize.png" alt="logo">
+        </div>
+
+        <div class="meio2">
+            <a href="paginaInicial.php">
+                <img id="casa1" src="../../assets/icons/casa.png" alt="casa">
+            </a>
+        </div>
+    </div>
+
+    <!-- Título: "Feedback" -->
+    <div>
+        <H2> FEEDBACK </H2>   
+    </div>
+
+  <br>
+
+    <!-- Todos os feedbacks das pessoas (nome da pessoa, quantidade de estrelas e a mensagem) --> 
+    <main>
+      <div class="branca">
+        <div id="flex9">
+            <H4>Jackson Oliveira</H4>
+            <img class= "estrelas"src="../../assets/icons/estrelas.png" alt="Quantidade de Estrelas">
+        </div>
+        <p>"Experiência incrível! O trem era muito confortável,
+            os assentos eram espaçosos e a viagem foi super
+            tranquila. Além disso, a pontualidade foi impecável.
+            Com certeza viajarei novamente!"</p>
+      </div>
+      <div class="branca">
+        <div id="flex9">
+            <H4>Otávio Ferreira </H4>
+            <img class="estrelas" src="../../assets/icons/estrelas.png" alt="Quantidade de Estrelas">
+        </div>
+        <p>"Ótimo serviço! A equipe de bordo foi muito
+            atenciosa, e o vagão-restaurante tinha boas opções
+            de comida. Só acho que poderia ter mais tomadas
+            para carregar o celular, mas no geral, foi uma
+            viagem excelente!"</p>
+      </div>
+      </div>
+      <div class="branca">
+        <div id="flex9">
+            <H4>Jaqueline Elisabeth</H4>
+            <img class="estrelas" src="../../assets/icons/estrelas.png" alt="Quantidade de Estrelas">
+        </div>
+        <p>"Viagem perfeita! O trem era silencioso e muito
+            limpo. Gostei bastante da paisagem ao longo do
+            trajeto. Cheguei ao destino no horário certo e sem
+            estresse. Super recomendo!"</p>
+
+      </div>
+      <div class="branca">
+        <div id="flex9">
+            <H4>Rodrigo Medeiros</H4>
+            <img  class= "estrelas" src="../../assets/icons/estrelas.png" alt="Quantidade de Estrelas">
+            
+        </div>
+        <p>"Gostei muito da experiência! O ar-condicionado
+            estava na temperatura ideal, e os assentos eram
+            confortáveis. A única coisa que poderia melhorar é
+            o Wi-Fi, que às vezes falhava. Fora isso, tudo
+            excelente!"</p>
+      </div>
+    </main>
+   
+</body>
+</html>
